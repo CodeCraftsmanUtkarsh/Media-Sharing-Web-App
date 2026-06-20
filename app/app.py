80e5617194ad332aafde915a8b0bebec03f1ece4
Utkarsh <utkarshsrivastava.1392006@gmail.com>
2026-06-20 20:37:11 +0530

    Post Method added

--- app/app.py
+++ app/app.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI,HTTPException
+from app.schemas import PostCreate
 app=FastAPI()
 text_posts={1: {"title": "New Post", "content": "Cool test post"},
 2: {"title": "Python Tip", "content": "Use list comprehensions for cleaner loops."},
@@ -20,3 +21,8 @@ def get_post_id(id:int):
     if id not in text_posts:
         raise HTTPException(status_code=404,detail="Post Not found")
     return text_posts.get(id)
+
+@app.post("/posts")
+def create_post(post:PostCreate):
+    text_posts[max(text_posts.keys())+1]={"title":PostCreate.title,"content":PostCreate.content}
+    
