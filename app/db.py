298a19188a2d89e919de8e7b8437dd2ee205f443
Utkarsh <utkarshsrivastava.1392006@gmail.com>
2026-06-26 11:48:53 +0530

    Fields of the db

--- app/db.py
+++ app/db.py
@@ -1,4 +1,5 @@
 from collections.abc import AsyncGenerator
+import datetime
 import uuid
 from sqlalchemy import Column,String,Text,DateTime,ForeignKey
 from sqlalchemy.dialects.postgresql import UUID
@@ -6,4 +7,13 @@ from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine,async_sessi
 from sqlalchemy.orm import DeclarativeBase, relationship 
 DATABASE_URL="sqlite+aiosqlite///./test.db"
 class Post(DeclarativeBase):
+    __tablename__="posts"
+    id=Column(UUID(as_uuid=True),primary_key=True,default=uuid.uuid4)
+    caption=Column(Text)
+    url=Column(String,nullable=False)
+    file_type=Column(String,nullable=False)
+    file_name=Column(String,nullable=False)
+    created_at=Column(DateTime,default=datetime.now)
+    
+    
     
